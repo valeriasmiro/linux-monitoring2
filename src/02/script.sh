72af38cc691c088ee38a7ae9654650aa3d1f5853
Valeria <valeriasmiro27@gmail.com>
2022-11-04 13:17:05 +0700

    02 completed

--- src/02/script.sh
+++ src/02/script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-path="/home/student/second_task/"
+path="/home/student/linux-monitoring2/trash/"
+if ! [ -d $path ]; then
+    mkdir $path
+fi
 declare -a files
-declare -a folders
 declare -a files_pattern_list
 declare -a folders_pattern_list
 
@@ -11,6 +13,7 @@ function files_pattern_to_list() {
     do
         files_pattern_list[i]=${files_name_pattern:i:1}
     done
+    
 }
 
 function folders_pattern_to_list() {
@@ -27,24 +30,22 @@ function files_num_generation() {
     return $files_num
 }
 
-#we generate 100 files name and put it to array
 function files_names_generation() {
-    #pointer and duplication parameters used for algoritm
-    ptr=0
-    dub=0
+    ptr_fi=0
+    dub_fi=0
 
     for (( i = 0; i < 100; i++ ))
     do
-        ptr=$((i%${#files_name_pattern}))
-        dub=$((2+i/${#files_name_pattern}))
+        ptr_fi=$((i%${#files_name_pattern}))
+        dub_fi=$((2+i/${#files_name_pattern}))
         name=""
 
         for (( j = 0; j < ${#files_name_pattern}; j++ ))
         do
-            if [[ j -eq $ptr ]]; then
-                dub=$(($dub-1))
+            if [[ j -eq $ptr_fi ]]; then
+                dub_fi=$(($dub_fi-1))
             fi
-            for (( k = 0; k < dub; k++ ))
+            for (( k = 0; k < dub_fi; k++ ))
             do
                 name+=${files_pattern_list[j]}
             done
@@ -54,49 +55,45 @@ function files_names_generation() {
     done
 }
 
-function folders_name_generation() {
-    ptr=0
-    dub=0
+function folder_name_generation() {
+    ptr_fo=0
+    dub_fo=0
     folders_pattern_length=${#folders_pattern}
 
-    for (( i = 0; i < 100; i++ ))
+    ptr_fo=$((i%$folders_pattern_length))
+    dub_fo=$((2+i/$folders_pattern_length))
+    name=""
+    for (( j = 0; j < folders_pattern_length; j++ ))
     do
-        ptr=$((i%$folders_pattern_length))
-        dub=$((2+i/$folders_pattern_length))
-        name=""
-
-        for (( j = 0; j < folders_pattern_length; j++ ))
+        if [[ j -eq $ptr_fo ]]; then
+        let "dub_fo -= 1"
+        fi
+        for (( k = 0; k < dub_fo; k++ ))
         do
-            if [[ j -eq $ptr ]]; then
-            let "dub -= 1"
-            fi
-            for (( k = 0; k < dub; k++ ))
-            do
-                name+=${folders_pattern_list[j]}
-            done
+            name+=${folders_pattern_list[j]}
         done
-        current_folder_name=$path$name$(date +"_%d%m%y/")
-        folders[i]=$current_folder_name
     done
+    current_folder_name=$path$name$(date +"_%d%m%y/")
 }
 
 function main_func() {
     delimeter="."
     files_name_pattern=${files_pattern%$delimeter*}
     files_extention=${files_pattern##*$delimeter}
-    echo $files_name_pattern
+
+    echo "This might take a little bit of time"
+    echo "be prepared that 1GB of free memory will remain on the disk"
 
     files_pattern_to_list
     folders_pattern_to_list
     files_names_generation
-    folders_name_generation
 
-    re='^[0]{1}\.[0-9]G$'
+    re='M$'
     for (( i = 0; i < 100; i++ ))
     do
-        current_folder_name=${folders[i]}
-        if [[ ! -d current_folder_name ]]; then
-            #mkdir $current_folder_name
+        folder_name_generation
+        if ! [[ -d $current_folder_name ]]; then
+            mkdir $current_folder_name
             echo $current_folder_name >> file_gen.log
         else
             echo $current_folder_name generated folder already exists! >> file_gen.log
@@ -108,17 +105,18 @@ function main_func() {
         for (( j = 0; j < files_num; j++))
         do
         free_memory=$(df -h | grep /dev/sda2 | awk '{print $4}')
-        if [[ ! $free_memory =~ $re ]]; then 
+        if ! [[ $free_memory =~ $re ]]; then 
             current_file_name=$current_folder_name${files[j]}
-            if [[ ! -f $current_file_name ]]; then
-            #fallocate -l ${file_size}M $current_file_name
-            to_log=$current_file_name$(date +" %d-%m-%Y %H:%M:%S ")$file_size"Mb"
-            echo $to_log >> file_gen.log
+            if ! [[ -f $current_file_name ]]; then
+                fallocate -l ${file_size}M $current_file_name
+                to_log=$current_file_name$(date +" %d-%m-%Y %H:%M:%S ")$file_size"Mb"
+                echo $to_log >> file_gen.log
             else
-            echo $current_file_name generated file already exists! >> file_gen.log
+                echo $current_file_name generated file already exists! >> file_gen.log
             fi
         else
             break
+            echo 'fuck you shit'
         fi
         done
     done
